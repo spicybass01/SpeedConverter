30b9f113ad5cf9d477a9413c0f8430c30d348e43
Ian Dunifon <ian.m.dunifon@gmail.com>
2019-04-28 12:36:48 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class SpeedConverter {
+    public static long toMilesPerHour(double kilometersPerHour){
+        if (kilometersPerHour < 0){
+            return -1;
+        } else {
+            return Math.round(kilometersPerHour / 1.609);
+        }
+    }
+    public static void printConversion(double kilometersPerHour){
+        if (kilometersPerHour < 0){
+            System.out.println("Invalid Value");
+        } else {
+            System.out.println(kilometersPerHour + " km/h = " + Math.round(kilometersPerHour / 1.609) + " mi/h");
+        }
+    }
+}
